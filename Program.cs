20dd7c1879d98eb0e968f859ffd1c7763a9cb8b4
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-12-08 20:08:50 -0500

    ListNode class created. static void Main created

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        return null;
+
+    }
+    public static void AfficherListe(ListNode head)
+    {
+        ListNode current = head;
+        while (current != null)
+        {
+            Console.Write(current.val + " -> ");
+            current = current.next;
+        }
+        Console.WriteLine("null");
+    }
+    
+    static void Main(string[]args)
+    {
+
+    }
+}
