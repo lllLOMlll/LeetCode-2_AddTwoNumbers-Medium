24d07d95dc5f61c719f589e533c325e8d962d169
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-12-08 20:52:35 -0500

    Reversing the first node into a List

--- Program.cs
+++ Program.cs
@@ -13,6 +13,25 @@
     }
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
+        ListNode current1 = l1;
+        ListNode current2 = l2;
+
+        List<int> numsListInNode1 = new List<int>();
+        List<int> invertNumsListInNode1 = new List<int>();
+        while (current1 != null)
+        {
+            numsListInNode1.Add(current1.val);
+            current1 = current1.next;
+        }
+
+        int lastIndexofnumsListNode1 =  numsListInNode1.Count -1; // Count - 1 = last index
+        for (int i = lastIndexofnumsListNode1; i > -1; i--)
+        {
+            invertNumsListInNode1.Add(numsListInNode1[i]); 
+        }
+        
+        Console.WriteLine(string.Join(", ", numsListInNode1));
+        Console.WriteLine(string.Join(", ", invertNumsListInNode1));
         return null;
 
     }
@@ -26,9 +45,16 @@
         }
         Console.WriteLine("null");
     }
-    
+
     static void Main(string[]args)
     {
+        Solution s = new Solution();
 
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        //AfficherListe(l1);
+
+        s.AddTwoNumbers(l1, l2);
     }
 }
